37b3375613b4cd97faf912cbfd801bafbb676cde
LuongAnh1 <93256715+LuongAnh1@users.noreply.github.com>
2025-04-17 19:20:35 +0700

    Add files via upload

--- BTL_Nhom4/main.c
+++ BTL_Nhom4/main.c
@@ -5,26 +5,12 @@
 #include "hash.h"
 #include "Data.h"
 #include "Manipulate.h"
+#include "Change_Data.h"
 #include <windows.h> // Thư viện cho hàm system("cls")
 
 Node *Table[TABLE_SIZE] = {NULL}; // Khai báo bảng băm toàn cục
 int main() {
-    FILE *f;
-    f=fopen("english_books.csv","r"); // Mở file
-    if (f == NULL) {
-        printf("Khong mo duoc file\n");
-        return 1;
-    }
-   
-    // Đọc file và chèn sách vào bảng băm
-    char line[100];
-    while (fgets(line, sizeof(line), f)) { // Đọc từng dòng trong file
-        Book book;
-        sscanf(line, "%[^,],%[^,],%d", book.Name, book.Author, &book.Have); // Phân tích cú pháp dòng
-        Insert(book); // Chèn sách vào bảng băm - hàm đã được định nghĩa ở file Manipulate.c
-    }
-    fclose(f); // Đóng file
-    printf("Tai du lieu thanh cong\n");
+    ReadDataFromFile("english_books.csv"); // Đọc dữ liệu từ file CSV và chèn vào bảng băm
     system("pause"); // Tạm dừng chương trình
     system("cls");
     return 0;
