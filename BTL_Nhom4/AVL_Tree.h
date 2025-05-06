250c4b6f51959d54e2f59860f1cd2e6703f26a40
LuongAnh1 <93256715+LuongAnh1@users.noreply.github.com>
2025-05-06 23:10:51 +0700

    đọc kỹ

--- BTL_Nhom4/AVL_Tree.h
+++ BTL_Nhom4/AVL_Tree.h
@@ -4,8 +4,8 @@
 
 typedef int (*CompareFunction)(void*, void*); 
 // cmp: hàm so sánh giữa 2 đối tượng, trả về 1 nếu lớn hơn, -1 nếu nhỏ hơn và 0 nếu bằng nhau
+// void *data: dữ liệu cần thêm vào cây AVL - tự định nghĩa kiểu dữ liệu
 AVLNode* insertAVL(AVLNode* root, void *data, CompareFunction cmp);
-AVLNode* deleteAVL(AVLNode* root, void *data, CompareFunction cmp);
 AVLNode* searchAVL(AVLNode* root, void *data, CompareFunction cmp);
 
 #endif
