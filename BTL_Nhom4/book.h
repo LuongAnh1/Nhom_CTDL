eb8c6c180971770e01938be66a5cc95743b93e58
dugonn <153439149+dugonn@users.noreply.github.com>
2025-05-17 15:46:13 +0700

    Update book.h

--- BTL_Nhom4/book.h
+++ BTL_Nhom4/book.h
@@ -2,16 +2,23 @@
 #define BOOK_H
 
 #include "data.h"
-#include "avlTree.h"
+#include "AVL_Tree.h"
 
-void loadBooksFromFile(const char* fileName);
+//Đọc dữ liệu sách từ file
+void loadBooksFromFile(const char *fileName);
 
+//Chèn thủ công
 void insertBook(Book book);
 
+//Tìm kiếm sách
 Book* searchBook(const char* title, const char* author);
 
+//Xóa sách
 void deleteBook(const char* title, const char* author);
 
+//Hiển thị toàn bộ thông tin sách trong thư viện
 void displayAllBooks();
 
+//Lưu vào file
+void saveToFile(const char *fileName);
 #endif
